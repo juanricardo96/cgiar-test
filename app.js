615d92ddc16fa019b0d01d3dfbc9f20cea900e6c
juanricardo96 <47462193+juanricardo96@users.noreply.github.com>
2021-07-06 04:21:25 -0500

    Clean JS

--- app.js
+++ app.js
@@ -18,33 +18,30 @@ const app = new Vue({
     },
     methods: {
         addPartner: function(){
-            console.log("branch", this.branch)
             if(this.branch != '' && this.name != '' && this.type != '' && this.country != '' && this.city != ''){
                 this.validado = true
             }
             if(this.validado===true){
-            this.partners.push({
-                sucursal: this.branch,
-                acronimo: this.acronym,
-                nombre: this.name,
-                tipo: this.type,
-                pais: this.country,
-                ciudad: this.city,
-                website: this.link
-            });
-            console.log("Agrego partner", this.partners);
-            localStorage.setItem('cgiar-vue', JSON.stringify(this.partners)); 
-            this.branch = '',
-            this.acronym = '',
-            this.name = '',
-            this.type = '',
-            this.country = '',
-            this.city = '',
-            this.link = ''
-            setTimeout ("window.location='partners.html'", 5000);
-        }else{
-            console.log("Ingrese datos restantes")
-        }
+                this.partners.push({
+                    sucursal: this.branch,
+                    acronimo: this.acronym,
+                    nombre: this.name,
+                    tipo: this.type,
+                    pais: this.country,
+                    ciudad: this.city,
+                    website: this.link
+                });
+                console.log("Agrego partner", this.partners);
+                localStorage.setItem('cgiar-vue', JSON.stringify(this.partners)); 
+                this.branch = '',
+                this.acronym = '',
+                this.name = '',
+                this.type = '',
+                this.country = '',
+                this.city = '',
+                this.link = ''
+                setTimeout ("window.location='partners.html'", 5000);
+            }  
         }
     },
     created: function(){
